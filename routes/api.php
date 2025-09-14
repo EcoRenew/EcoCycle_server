fe03495f25b2e006e77ef767fc7c6382f5d4d4b0
K-nass <karimnase10@gmail.com>
2025-09-14 16:51:01 +0300

    added static images for products

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,24 @@
-<?php
+    <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\StripeController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\AuthController;
+    use App\Http\Controllers\ProductController;
+    use App\Http\Controllers\StripeController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-
-Route::post('users/register', [AuthController::class, 'register']);
-Route::post('users/login', [AuthController::class, 'login']);
-Route::post('users/logout', [AuthController::class, 'logout'])
-    ->middleware('auth:sanctum');
+    // Route::get('/user', function (Request $request) {
+    //     return $request->user();
+    // })->middleware('auth:sanctum');
 
 
-Route::post('/products/{product}/produce', [ProductController::class, 'produce']);
-Route::apiResource('products', ProductController::class);
+    Route::post('users/register', [AuthController::class, 'register']);
+    Route::post('users/login', [AuthController::class, 'login']);
+    Route::post('users/logout', [AuthController::class, 'logout'])
+        ->middleware('auth:sanctum');
 
-Route::post('/products/pay', [StripeController::class, 'pay'])
-    ->middleware('auth:sanctum');
+
+    Route::post('/products/{product}/produce', [ProductController::class, 'produce']);
+    Route::apiResource('products', ProductController::class);
+
+    Route::post('/products/pay', [StripeController::class, 'pay'])
+        ->middleware('auth:sanctum');
