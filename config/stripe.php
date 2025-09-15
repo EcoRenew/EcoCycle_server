744aae7261037a7fa33f9e9d5b01f2fa3fccf907
K-nass <karimnase10@gmail.com>
2025-09-15 16:37:50 +0300

    added stripe web hooks

--- config/stripe.php
+++ config/stripe.php
@@ -3,6 +3,5 @@ return [
     'api_key' => [
         'secret' => env('STRIPE_SECRET')
     ],
-    'success_url' => env('STRIPE_SUCCESS_URL', config('app.url') . '/success'),
-    'cancel_url' => env('STRIPE_CANCEL_URL', config('app.url') . '/cancel'),
+    'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
 ];
