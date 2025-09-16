b4ff9b3a3394712a4c3abebf69097b06fb158d0b
K-nass <karimnase10@gmail.com>
2025-09-16 19:36:42 +0300

    added local sucess url

--- config/stripe.php
+++ config/stripe.php
@@ -1,7 +1,9 @@
 <?php
 return [
     'api_key' => [
-        'secret' => env('STRIPE_SECRET')
+        'secret' => env('STRIPE_SECRET'),
     ],
     'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
+
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:5173'),
 ];
