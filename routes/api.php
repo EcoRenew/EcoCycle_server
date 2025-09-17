fcbec5f51da27e0fe5e73ce2fb71d01fb9ff6555
narriman <narimanamer805@gmail.com>
2025-09-17 13:40:08 +0300

    adding ai api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StripeController;
 use App\Http\Controllers\StripeWebHook;
+use App\Http\Controllers\AIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,5 @@ Route::apiResource('products', ProductController::class);
 Route::post('stripe/webhook', [StripeWebHook::class, 'handleWebHook']);
 Route::post('/products/pay', [StripeController::class, 'pay']);
 
+Route::post('/ai/diy-helper', [AIController::class, 'generateDIY']);
+// Route::match(['get', 'post'], '/ai/diy-helper', [AIController::class, 'generateDIY']);
