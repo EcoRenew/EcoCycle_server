e92dc7389459d6a43aa5e297f449059dd634a852
AbdulRahmanKhawaga <127113523+AbdulRahmanKhawaga@users.noreply.github.com>
2025-09-17 21:22:21 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,26 +3,27 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Fruitcake\Cors\HandleCors;
 use App\Http\Middleware\RoleMiddleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(_DIR_))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: _DIR_.'/../routes/web.php',
+        api: _DIR_.'/../routes/api.php',
+        commands: _DIR_.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-
+        // Define middleware aliases
         $middleware->alias([
             'role' => RoleMiddleware::class,
+        ]);
 
+        // Exclude specific URIs from CSRF protection
         $middleware->validateCsrfTokens(except: [
             '/webhook/stripe',
-
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        // Customize exception handling here if needed
+    })
+    ->create();
