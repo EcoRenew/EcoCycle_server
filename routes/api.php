3cc8bd37fa3ceb7b2fa0161aea140dacda5f059b
K-nass <karimnase10@gmail.com>
2025-09-18 15:46:35 +0300

    added verfy to registered emails

--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,26 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StripeController;
 use App\Http\Controllers\StripeWebHook;
 use App\Http\Controllers\AIController;
-use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\EnsureEmailIsVerified;
+use Illuminate\Auth\Events\Verified;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
+Route::get('/email/verify/{id}/{hash}', function ($id, $hash) {
+    $user = User::findOrFail($id);
+
+    if (!hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
+        return redirect(env('FRONTEND_URL') . '/register'); 
+    }
+
+    if (!$user->hasVerifiedEmail()) {
+        $user->markEmailAsVerified();
+        event(new Verified($user));
+    }
+    return redirect(env('FRONTEND_URL') . '/login');
+
+})->middleware('signed')->name('verification.verify');
 
 Route::post('users/register', [AuthController::class, 'register']);
 Route::post('users/login', [AuthController::class, 'login']);
@@ -26,7 +39,7 @@ Route::apiResource('products', ProductController::class);
 
 
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum',EnsureEmailIsVerified::class])->group(function () {
     Route::get('/cart', [CartController::class, 'index']);
     Route::post('/cart', [CartController::class, 'store']);
     Route::put('/cart/{id}', [CartController::class, 'update']);
